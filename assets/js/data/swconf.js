0ead697cdc252b1706580b952453a52c422ab2ba
SaeidSec <contact@saidecsec.com>
2026-05-30 10:29:10 +0600

    Deploy static site to gh-pages

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1780115332',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/services/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/resume/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
